80b59311642488541e52c77feee4a9f917712c1c
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-08-15 15:45:39 +0300

    Add section challenge

---
+++ sectionChallenge/sectionChallenge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sectionChallenge
+//
+//  Created by Maged Alosali on 15/08/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
