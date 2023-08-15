f8fc1a28bebff912e9ce3640e0834294afe94c4d
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-08-15 21:05:40 +0300

    Finish section challenge

--- sectionChallenge/sectionChallenge/main.cpp
+++ sectionChallenge/sectionChallenge/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  sectionChallenge
-//
-//  Created by Maged Alosali on 15/08/2023.
-//
+
 
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    cout<< "Hello, welcome to Maged's Carpet Cleaning Service" << endl;
+    
+    cout << "\nHow many small rooms would you like cleaned? ";
+    int smallRooms {0};
+    cin >> smallRooms;
+    
+    cout << "How many large rooms would you like cleaned? ";
+    int largeRooms {0};
+    cin >> largeRooms;
+    
+    cout << "\nEstimate for carpet cleaning service";
+    cout << "\nNumber of small rooms: " << smallRooms;
+    cout << "\nNumber of large rooms: " << largeRooms;
+    
+    const double smallCost {25};
+    cout << "\nPrice per small room: $" <<smallCost;
+    
+    const double largeCost {35};
+    double roomsCost = (smallCost * smallRooms) + (largeCost * largeRooms);
+    double tax = roomsCost * 0.06;
+    
+    cout << "\nCost: $" << roomsCost;
+    cout << "\nTax: $" << tax;
+    
+    cout << "\n===============================================";
+    cout << "\nTotal cost: " << roomsCost + tax;
+    const int days = 30;
+    cout << "\nThis cost is valid for " << days << " days" << endl;
+    
     return 0;
 }
